f1111ae36926b9ad6525f482fe22fb737f12f726
Tony Cui <tacui@mit.edu>
2023-01-23 15:45:41 +0200

    clicker

--- game.js
+++ game.js
@@ -1,65 +1,20 @@
-var max_number = 10;
-
-if(document.getElementById("title").innerHTML == "Hard Mode"){
-    max_number = 200;
-}
-
 var score = 0;
 
-getNewProblem();
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
+var up = document.getElementById("up");
+var reset = document.getElementById("reset");
 
-function getNewProblem(){
-    console.log("help")
-    var correct = document.getElementById("correct");
-    var wrong = document.getElementById("wrong");
-    var prompt = document.getElementById("prompt");
+up.addEventListener("click", increaseScore);
+reset.addEventListener("click", resetScore);
 
-    correct.removeEventListener("click", correctAnswer);
-    correct.removeEventListener("click", gameOver);
-    wrong.removeEventListener("click", correctAnswer);
-    wrong.removeEventListener("click", gameOver);
 
-    var num1 = getRandomInt(max_number);
-    var num2 = getRandomInt(max_number);
-
-    const ADJUST = [-1, 0, 1]
-
-    var displayed_sol = num1 + num2 + ADJUST[getRandomInt(2)];
-
-    prompt.innerHTML = `${num1} + ${num2} = ${displayed_sol}`
-
-    if (num1 + num2 == displayed_sol){ // prompt is correct
-        correct.addEventListener("click", correctAnswer);
-        wrong.addEventListener("click", gameOver);
-    } else { // prompt isn't correct
-        correct.addEventListener("click", gameOver);
-        wrong.addEventListener("click", correctAnswer);
-    }
-}
-
-function correctAnswer() {
-    getNewProblem();
-    score++;
+function increaseScore(){
+    score = score + 1;
     document.getElementById('score').innerHTML = `Score: ${score}`
+
 }
 
-function gameOver(){
-    
-
-    console.log("game over")
-    var correct = document.getElementById("correct");
-    var wrong = document.getElementById("wrong");
-    var prompt = document.getElementById("prompt");
-    
-    prompt.innerHTML = `game over`
-
-    correct.removeEventListener("click", correctAnswer);
-    correct.removeEventListener("click", gameOver);
-    wrong.removeEventListener("click", correctAnswer);
-    wrong.removeEventListener("click", gameOver);
-
+function resetScore(){
+    score = 0;
+    document.getElementById('score').innerHTML = `Score: ${score}`
 }
